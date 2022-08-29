f6658a16d00512dded6430e06f0c7b45ed701126
Anoop Sharma <anoop@noblefive.com>
2022-08-29 18:49:42 +0530

    like button

--- components/NFTCard.js
+++ components/NFTCard.js
@@ -1,7 +1,7 @@
 import { View, Image, Text } from "react-native";
 import { useNavigation } from "@react-navigation/native";
-
 import { COLORS, SIZES, SHADOWS, assets } from "../constants";
+import { CircleButton } from "./index";
 
 const NFTCard = ({ data }) => {
   const navigation = useNavigation();
@@ -27,8 +27,8 @@ const NFTCard = ({ data }) => {
             borderTopRightRadius: SIZES.font,
           }}
         />
+        <CircleButton imgUrl={assets.heart} right={10} top={10} />
       </View>
-      <Text>NFTCard</Text>
     </View>
   );
 };
