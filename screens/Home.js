0ad715f5a07eb7a5ef443542ef9e759db40abc4c
Anoop Sharma <anoop@noblefive.com>
2022-08-29 17:51:05 +0530

    home screen

--- screens/Home.js
+++ screens/Home.js
@@ -1,7 +1,40 @@
-import { Text } from "react-native";
+import { useState } from "react";
+import { Text, View, SafeAreaView, FlatList } from "react-native";
+import { COLORS, NFTData } from "../constants";
+import { NFTCard, HomeHeader, FocusedStatusBar } from "../components";
 
 const Home = () => {
-  return <Text>Home</Text>;
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <FocusedStatusBar backgroundColor={COLORS.primary} />
+
+      <View style={{ flex: 1 }}>
+        <View style={{ zIndex: 0 }}>
+          <FlatList
+            data={NFTData}
+            renderItem={({ item }) => <Text>{item.name}</Text>}
+            keyExtractor={(item) => item.id}
+            showsHorizontalScrollIndicator={false}
+            ListHeaderComponent={<HomeHeader></HomeHeader>}
+          />
+        </View>
+
+        <View
+          style={{
+            position: "absolute",
+            top: 0,
+            bottom: 0,
+            right: 0,
+            left: 0,
+            zIndex: -1,
+          }}
+        >
+          <View style={{ height: 300, backgroundColor: COLORS.primary }} />
+          <View style={{ flex: 1, backgroundColor: COLORS.white }} />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
 };
 
 export default Home;
